cbc731f6b487214cd5d81f44d4f46f585019512b
siva <shivajstme@gmail.com>
2020-09-20 01:03:40 +1000

    Added All Cards

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,20 +1,45 @@
 import React from "react";
 import CardItem  from "./CardsItem";
+import imag from "../../assets/images/img-9.jpg";
+import "./Card.css";
+import {CardData} from "./CardData";
+
 
 function Cards() {
+    console.log(CardData[0].src);
+    const CardComponent = CardData.map((user,i) =>{
+            return (
+                <CardItem
+                    key={i}
+                    src={user.src}
+                    text={user.text}
+                    label={user.label}
+                    path={user.path}
+                />
+            );
 
+
+
+    })
     return(
         <div className="cards">
             <h1>Check out these EPIC Destinations!</h1>
             <div className="cards__container">
                 <div className="cards__wrapper">
+
                     <ul className="cards__items">
+                        {
+                        /* 
                         <CardItem
-                        src = "images/img-9.jpg"
+                            src={imag}
                         text= "Explore the hidden waterfall deep inside Amazon Jungle"
                         label = "Adventure"
                         path = "/services"
-                        />
+                        /> 
+                        */
+                        }
+                    {CardComponent}
+                        
                     </ul>
                 </div>
             </div>
---
+++ src/Components/Card/CardData.js
@@ -0,0 +1,43 @@
+
+export const CardData = [
+    {
+        id  : 1,
+        src: require("../../assets/images/img-9.jpg"),
+        text : "Explore the hidden waterfall deep inside Amazon Jungle",
+        label : "Adventure",
+        path  : "/services"
+
+    },
+    {
+        id: 2,
+        src: require("../../assets/images/img-2.jpg"),
+        text: "Travel through the Islands of Bali in a Private Cruise",
+        label: "luxury",
+        path: "/services"
+
+    },
+    {
+        id: 3,
+        src: require("../../assets/images/img-3.jpg"),
+        text: "Set Sail in the Atlantic Ocean visiting",
+        label: "Mystery",
+        path: "/services"
+
+    },
+    {
+        id: 4,
+        src: require("../../assets/images/img-4.jpg"),
+        text: "Experience Football on Top of the Himalayas",
+        label: "Adventure",
+        path: "/products"
+
+    },
+    {
+        id: 5,
+        src: require("../../assets/images/img-8.jpg"),
+        text: "Ride through the Sahara Desert on a guided camel tour",
+        label: "Adrenaline",
+        path: "/services"
+    }
+
+]
